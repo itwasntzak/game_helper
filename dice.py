96c3b4eb8b55f3ae5e1636d110a500bb89d068a3
itwasntzak <51720888+itwasntzak@users.noreply.github.com>
2019-07-05 13:15:44 -0600

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,14 +1,12 @@
 import random
 
 def dice():
-    a = random.randrange(1, 7, 1)
-    return(a)
+    diceRoll = random.randrange(1, 7, 1)
+    return(diceRoll)
 
-def dice_roll():
-    dice_number = input()
-    dice_store = []
-    while int(dice_number) > len(dice_store):
-        dice_store.append(dice())
-        if int(dice_number) <= len(dice_store):
-            break
-    print(dice_store)
+def diceRoll():
+    userChoice = int(input())
+    rollResult = []
+    for value in range(userChoice):
+        rollResult.append(dice())
+    print(rollResult)
